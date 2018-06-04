95ba01066111ee6fbfc4fa8b91843c510409f1af
Guilherme Carvalho Pereira <37717912+GuilhermeCarvalho1144@users.noreply.github.com>
2018-06-04 12:54:17 -0300

    Add files via upload

---
+++ Lesson_2/Twenty_Second _quiz.py
@@ -0,0 +1,54 @@
+#==============================================================================
+# THIS CODE IMPLEMENTS THE TWENTY SECOND QUIZ FROM INDTRODUCTION TO DATA ANALYSIS COURSE
+# AUTHOR : GUILHERME CARVALHO PEREIRA
+# SOURCE: INDTRODUCTION TO DATA ANALYSIS COURSE...UDACITY COURSE
+#============================================================================
+##START
+##LIBRARIES
+import pandas as pd
+#==============================================================================
+def reverse_names(names):
+    '''
+    Fill in this function to return a new series where each name
+    in the input series has been transformed from the format
+    "Firstname Lastname" to "Lastname, FirstName".
+    
+    Try to use the Pandas apply() function rather than a loop.
+    '''
+    first_name = []
+    second_name = []
+    first_name, second_name = names.split(" ")
+    aux = second_name +', '+ first_name
+    
+    return pd.Series(aux)
+#==============================================================================
+# CREATING THE PANDAS SERIES
+names = pd.Series([
+    'Andre Agassi',
+    'Barry Bonds',
+    'Christopher Columbus',
+    'Daniel Defoe',
+    'Emilio Estevez',
+    'Fred Flintstone',
+    'Greta Garbo',
+    'Humbert Humbert',
+    'Ivan Ilych',
+    'James Joyce',
+    'Keira Knightley',
+    'Lois Lane',
+    'Mike Myers',
+    'Nick Nolte',
+    'Ozzy Osbourne',
+    'Pablo Picasso',
+    'Quirinus Quirrell',
+    'Rachael Ray',
+    'Susan Sarandon',
+    'Tina Turner',
+    'Ugueth Urbina',
+    'Vince Vaughn',
+    'Woodrow Wilson',
+    'Yoji Yamada',
+    'Zinedine Zidane'
+])
+result = names.apply(reverse_names)
+print result
